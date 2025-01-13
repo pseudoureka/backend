4a36d452798224f8669fa915563d9a745c87ead9
pseudoureka <messiah7179@gmail.com>
2025-01-13 13:05:28 +0900

    쿼리스트링 처리

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ import tasks from "./data/mock.js";
 const app = express();
 
 app.get("/tasks", (req, res) => {
-  res.send(tasks);
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  const compareFn =
+    sort === "oldest" ? (a, b) => a.createdAt - b.createdAt : (a, b) => b.createdAt - a.createdAt;
+
+  let newTasks = tasks.sort(compareFn);
+
+  if (count) {
+    newTasks = tasks.slice(0, count);
+  }
+
+  res.send(newTasks);
 });
 
 app.listen(3000, () => {
