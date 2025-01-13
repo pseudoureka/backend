ece185677d44f7ba07173cd6c5153b9e794ca083
pseudoureka <messiah7179@gmail.com>
2025-01-13 17:53:47 +0900

    POST, PATCH 리퀘스트 코드 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import subscriptions from "./data/mock.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/subscriptions", (req, res) => {
   const sort = req.query.sort;
@@ -11,6 +12,48 @@ app.get("/subscriptions", (req, res) => {
 
   const newSubscriptions = subscriptions.sort(compareFn);
   res.send(newSubscriptions);
+});
+
+app.get("/subscriptions/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const subscription = subscriptions.find((sub) => sub.id === id);
+
+  if (subscription) {
+    res.send(subscription);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
+});
+
+function getNextId(arr) {
+  const ids = arr.map((elt) => elt.id);
+  return Math.max(...ids) + 1;
+}
+
+app.post("/subscriptions", (req, res) => {
+  const newSubscription = req.body;
+
+  newSubscription.id = getNextId(subscriptions);
+  newSubscription.createdAt = new Date();
+  newSubscription.updatedAt = new Date();
+
+  subscriptions.push(newSubscription);
+  res.status(201).send(newSubscription);
+});
+
+app.patch("/subscriptions/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const subscription = subscriptions.find((sub) => sub.id === id);
+
+  if (subscription) {
+    Object.keys(req.body).forEach((key) => {
+      subscription[key] = req.body[key];
+    });
+    subscription.updatedAt = new Date();
+    res.send(subscription);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
 });
 
 app.listen(3000, () => {
